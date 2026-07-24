1048ea0352aa9d22215fe7678e7a2b54abcca196
Agrim Singhal <ogagrim7@gmail.com>
2026-07-24 15:12:20 +0530

    realexample

--- lab1/eventEmitter.js
+++ lab1/eventEmitter.js
@@ -20,6 +20,13 @@ task.on("greet", login);
 task.on("greet", working);
 task.on("greet", checkout);
 
+task.once("exit", (name) => {
+  console.log(`${name} is shutting down`);
+});
+
 task.emit("greet", "Agrim Singhal");
 task.emit("greet", "Aditya verma");
+task.off("greet", working);
 task.emit("greet", "Anav Singhal");
+task.emit("exit","Manager");
+//nameless fucntion    ()=>{}
