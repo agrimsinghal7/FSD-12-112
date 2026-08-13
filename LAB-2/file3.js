a4ebe76106de01055d19306e2746d079b429199d
Agrim Singhal <ogagrim7@gmail.com>
2026-08-13 12:00:52 +0530

    use of stat and access value of fstat

---
+++ LAB-2/file3.js
@@ -0,0 +1,11 @@
+import { stat } from "fs/promises";
+
+const fstat = await stat("file1.js");
+
+console.log("file size:", fstat.size, "bytes");
+
+console.log(`is file: ${fstat.isFile()}`);
+console.log(`is folder: ${fstat.isDirectory()}`);
+console.log(`is syslink: ${fstat.isSymbolicLink()}`);
+//symbolic link meaning is this value of an operating system ?
+//Difference between folder and file? 
